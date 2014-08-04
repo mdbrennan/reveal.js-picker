72e5df78e90ff61db3486787199baae6d42d8953
Matthew Brennan <matthew.brennan@abc.net.au>
2014-08-04 14:40:26 +1000

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		// Import package manifest
+		pkg: grunt.file.readJSON("reveal.js.picker.json"),
+
+		// Banner definitions
+		meta: {
+			banner: "/*\n" +
+				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+				" *  <%= pkg.description %>\n" +
+				" *  <%= pkg.homepage %>\n" +
+				" *\n" +
+				" *  Made by <%= pkg.author.name %>\n" +
+				" *  Under <%= pkg.licenses[0].type %> License\n" +
+				" */\n"
+		},
+
+		// Concat definitions
+		concat: {
+			dist: {
+				src: ["src/reveal.js.picker.js"],
+				dest: "dist/reveal.js.picker.js"
+			},
+			options: {
+				banner: "<%= meta.banner %>"
+			}
+		},
+
+		// Lint definitions
+		jshint: {
+			files: ["src/jquery.boilerplate.js"],
+			options: {
+				jshintrc: ".jshintrc"
+			}
+		},
+
+		// Minify definitions
+		uglify: {
+			my_target: {
+				src: ["dist/reveal.js.picker.js"],
+				dest: "dist/reveal.js.picker.min.js"
+			},
+			options: {
+				banner: "<%= meta.banner %>"
+			}
+		},
+		
+		
+		// watch for changes to source 
+		// Better than calling grunt a million times 
+		// (call 'grunt watch')
+		watch: {
+		    files: ['src/*'],
+		    tasks: ['default']
+		}
+
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+
+	grunt.registerTask("default", ["jshint", "concat", "uglify"]);
+	grunt.registerTask("travis", ["jshint"]);
+
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+##Reveal.js Picker
+Picker Plugin for reveal.js.
+
+Adds a select to page in order to choose which slideshow to run
